f9b3fce6cb140b4e8f8854f4562d01259ed6cf20
Snowep <snowep66@live.com>
2018-09-16 20:49:55 +0700

    Update
    Change pull to float

--- superuser/element/footer.php
+++ superuser/element/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-  <div class="pull-right hidden-xs">
+  <div class="float-right hidden-xs">
     <b>Version</b> 0.0.1
   </div>
   <strong>Copyright &copy; 2018. All rights reserved.
