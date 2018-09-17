3f52668c230271073268705bfc6bd8a1d892673f
beautifulsky11 <42290852+beautifulsky11@users.noreply.github.com>
2018-09-18 03:59:37 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,211 @@
+<header class="main-header">
+
+  <!-- Logo -->
+  <a href="index.php" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>S</b>I</span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><b>Sistem</b>Inventaris</span>
+  </a>
+
+  <!-- Header Navbar: style can be found in header.less -->
+  <nav class="navbar navbar-static-top" style="height: 51px;">
+    <!-- Sidebar toggle button-->
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <i class="fa fa-bars"></i>
+    </a>
+    <!-- Navbar Right Menu -->
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        <!-- Messages: style can be found in dropdown.less-->
+        <li class="user user-menu">
+          <a href="../login.php">
+            Sign Out
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel">
+      <div class="float-left image">
+        <img src="../dist/img/user2-160x160.jpg" class="rounded-circle" alt="User Image">
+      </div>
+      <div class="float-left info">
+        <p>Alexander Pierce</p>
+        <a href="#"><i class="fa fa-circle text-success"></i> Superuser</a>
+      </div>
+    </div>
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu" data-widget="tree">
+      <li class="header">Navigation</li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-building"></i> <span>Kantor</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left float-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+      <?php
+        $sql = $db->query("SELECT * FROM kantor");
+        $count = $sql->rowCount();
+        if ($count > 0) {
+          while ($row = $sql->fetch()) {
+      ?>
+          <li><a href="kantor.php?id_kantor=<?php echo $row['id_kantor'] ?>"><?php echo $row['nama_kantor'] ?></a></li>
+      <?php
+          }
+        } else {
+      ?>
+        <li><a href="#">Tidak ada data Kantor</a></li>
+      <?php
+        }
+      ?>
+          <li><a href="tambah_kantor.php"><i class="fa fa-plus"></i> Tambah Kantor</a></li>
+
+        </ul>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-box"></i>
+          <span>Aset</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left float-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="PC.php"><i class="fa fa-desktop"></i> PC</a></li>
+          <li><a href="server.php"><i class="fa fa-server"></i> Server</a></li>
+          <li><a href="switch.php"><i class="fa fa-microchip"></i> Switch</a></li>
+          <li><a href="wifi.php"><i class="fa fa-wifi"></i> WiFi</a></li>
+        </ul>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-laptop"></i>
+          <span>UI Elements</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left float-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
+          <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
+          <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
+          <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
+          <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
+          <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
+        </ul>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i> <span>Forms</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left float-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="process/input_server.php"><i class="fa fa-circle-o"></i> Input Asset Server</a></li>
+          <li><a href="process/input_wifi.php"><i class="fa fa-circle-o"></i> Input Asset Wifi</a></li>
+          <li><a href="process/input_pc.php"><i class="fa fa-circle-o"></i> Input Asset Komputer</a></li>
+          <li><a href="process/input_switch.php"><i class="fa fa-circle-o"></i> Input Asset Switch</a></li>
+        </ul>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-table"></i> <span>Tables</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left float-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
+          <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="pages/calendar.html">
+          <i class="fa fa-calendar"></i> <span>Calendar</span>
+          <span class="pull-right-container">
+            <small class="badge pull-right bg-red">3</small>
+            <small class="badge pull-right bg-blue">17</small>
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="pages/mailbox/mailbox.html">
+          <i class="fa fa-envelope"></i> <span>Mailbox</span>
+          <span class="pull-right-container">
+            <small class="badge pull-right bg-yellow">12</small>
+            <small class="badge pull-right bg-green">16</small>
+            <small class="badge pull-right bg-red">5</small>
+          </span>
+        </a>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-folder"></i> <span>Examples</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left float-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="pages/examples/invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
+          <li><a href="pages/examples/profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
+          <li><a href="pages/examples/login.html"><i class="fa fa-circle-o"></i> Login</a></li>
+          <li><a href="pages/examples/register.html"><i class="fa fa-circle-o"></i> Register</a></li>
+          <li><a href="pages/examples/lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
+          <li><a href="pages/examples/404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
+          <li><a href="pages/examples/500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
+          <li><a href="pages/examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
+          <li><a href="pages/examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
+        </ul>
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-share"></i> <span>Multilevel</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left float-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+          <li class="treeview">
+            <a href="#"><i class="fa fa-circle-o"></i> Level One
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left float-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
+              <li class="treeview">
+                <a href="#"><i class="fa fa-circle-o"></i> Level Two
+                  <span class="pull-right-container">
+                    <i class="fa fa-angle-left float-right"></i>
+                  </span>
+                </a>
+                <ul class="treeview-menu">
+                  <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                  <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+        </ul>
+      </li>
+      <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+      <li class="header">badgeS</li>
+      <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+      <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+      <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+    </ul>
+  </section>
+  <!-- /.sidebar -->
+</aside>
