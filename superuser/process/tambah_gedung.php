0fa57160bd327ab4291fc245678b36fce08aa39c
Snowep <snowep66@live.com>
2018-09-19 12:39:26 +0700

    Up

--- superuser/process/tambah_gedung.php
+++ superuser/process/tambah_gedung.php
@@ -7,6 +7,6 @@
   $al_gedung = $_POST['alamat_gedung'];
   echo $id;
   $sql = $db->query("INSERT INTO gedung(id_kantor, nama_gedung, alamat) VALUES('$id','$nm_gedung','$al_gedung')");
-
+ 
   header("location:../kantor.php?id_kantor=$id");
 ?>
