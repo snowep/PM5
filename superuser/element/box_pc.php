8be7c77630224f179c0db9bf898053eeaf68f75a
Snowep <snowep66@live.com>
2018-09-23 00:59:58 +0700

    Change from Row to Box.
    More specific

---
+++ superuser/element/box_pc.php
@@ -0,0 +1,45 @@
+<div class="box">
+  <div class="box-header with-border">
+    <h3 class="box-title">Daftar PC</h3>
+
+    <div class="box-tools pull-right">
+      <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#tambahPC"><i class="fa fa-plus"></i> Tambah Aset</button>
+    </div>
+  </div>
+  <div class="box-body">
+    <div class="row">
+      <div class="card-columns">
+    <?php
+        while ($row = $sqlPc->fetch()) {
+    ?>
+        <div class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">
+              <?php echo strtoupper($row['jenis'])." | ".$row['processor'] ?>
+            </h5>
+            <p><?php echo $row['ip_address'] ?></p>
+            <?php
+              $query = $db->query("SELECT * FROM pegawai WHERE id_pc = '".$row['id_pc']."'");
+              $data = $query->fetch();
+              if ($data['id_pc'] !== $row['id_pc']) {
+            ?>
+            <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#tambahUser<?php echo $row['id_pc'] ?>"><i class="fa fa-user"></i> Belum ada User</button>
+            <?php
+              }
+            ?>
+            <a href="pc_detail.php?id_pc=<?php echo $row['id_pc'] ?>" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="bottom" title="Detail Gedung"><i class="fa fa-layer-group"></i></a>
+            <span data-toggle="modal" data-target="#hapusPC<?php echo $row['id_pc'] ?>" >
+              <button type="button" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="bottom" title="Hapus Gedung"><i class="fa fa-trash"></i></button>
+            </span>
+          </div>
+        </div>
+          <?php
+              }
+          ?>
+
+        </div>
+    </div>
+  </div>
+  <!-- /.box-header -->
+  <!-- ./box-body -->
+</div>
