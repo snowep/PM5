0dd642225bb69b2ae643d3d83761982a0c66631c
Snowep <snowep66@live.com>
2018-09-23 01:02:01 +0700

    Change

--- superuser/wifi.php
+++ superuser/wifi.php
@@ -282,52 +282,6 @@
 <script src="../dist/js/pages/dashboard2.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../dist/js/demo.js"></script>
-<script type="text/javascript">
-  $(document).ready(function() {
-    $('#kantor').on('change', function() {
-      var idKantor = $(this).val();
-      if (idKantor) {
-        $.ajax({
-          type: 'POST',
-          url:  'ajaxData.php',
-          data: 'id_kantor='+idKantor,
-          success:function(html) {
-            $('#gedung').html(html);
-            $('#lantai').html('<option value="">Pilih Gedung Dahulu</option>')
-          }
-        })
-      }
-    });
-
-    $('#gedung').on('change', function() {
-      var idGedung = $(this).val();
-      if (idGedung) {
-        $.ajax({
-          type: 'POST',
-          url:  'ajaxData.php',
-          data: 'id_gedung='+idGedung,
-          success:function(html) {
-            $('#lantai').html(html);
-            $('#ruangan').html('<option value="">Pilih Lantai Dahulu</option>')
-          }
-        })
-      }
-    });
-
-    $('#lantai').on('change', function() {
-      var idLantai = $(this).val();
-      if (idLantai) {
-        $.ajax({
-          type: 'POST',
-          url:  'ajaxData.php',
-          data: 'id_lantai='+idLantai,
-          success:function(html) {
-            $('#ruangan').html(html);
-          }
-        })
-      }
-    });
-  })
-</script>
+<?php include 'ajaxGetData.php'; ?>
 </body>
 </html>
